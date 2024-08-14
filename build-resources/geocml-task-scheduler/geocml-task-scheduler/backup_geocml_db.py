87cdbc79d695b1652b6ff7668368a903dabb3e3f
Tristan Damron <tristandamron@protonmail.com>
2024-08-13 17:00:32 -0700

    Use Tabor for DBBackups

--- build-resources/geocml-task-scheduler/geocml-task-scheduler/backup_geocml_db.py
+++ build-resources/geocml-task-scheduler/geocml-task-scheduler/backup_geocml_db.py
@@ -1,37 +1,52 @@
 import psycopg2
 import os
+import subprocess
 from time import time
 from task_logger import log
 
-ignore_tables = ('spatial_ref_sys', 'geometry_columns', 'geography_columns')
-ignore_schemas = ('pg_catalog', 'information_schema')
+ignore_tables = ("spatial_ref_sys", "geometry_columns", "geography_columns")
+ignore_schemas = ("pg_catalog", "information_schema")
 
 def backup_geocml_db():
     try:
-        conn = psycopg2.connect(dbname='geocml_db',
-                                user='postgres',
-                                password='admin',
-                                host='geocml-postgres',
+        conn = psycopg2.connect(dbname="geocml_db",
+                                user="geocml",
+                                password="geocml",
+                                host="geocml-postgres",
                                 port=5432)
     except psycopg2.OperationalError:
-        log('Couldn\'t connect to geocml_db; is the postgresql service started?')
+        log("Couldn\'t connect to geocml_db; is the postgresql service started?")
+        return
+
+    back_up_timestamp = time()
+    path_to_backup_dir = os.path.join(os.sep, "DBBackups", str(back_up_timestamp))
+    os.mkdir(path_to_backup_dir)
+    delete_backup_dir = True
+
+    # Write table schemata to .tabor file
+    out = subprocess.run(["tabor", "write", "--db", "geocml_db",
+                             "--username", "postgres", "--password", "admin",
+                             "--host", "geocml-postgres",
+                             "--file", os.path.join(path_to_backup_dir, "geocml_db.tabor")],
+                             capture_output=True)
+
+    if out.stderr:
+        log("Failed to generate .tabor file {}".format(out.stderr))
+        os.rmdir(path_to_backup_dir)
         return
 
     cursor = conn.cursor()
-    cursor.execute('SELECT DISTINCT table_schema FROM information_schema.tables;')
+    cursor.execute("""SELECT DISTINCT table_schema FROM information_schema.tables;""")
     schemas = cursor.fetchall()
-    back_up_timestamp = time()
-    path_to_backup_dir = os.path.join(os.sep, 'DBBackups', str(back_up_timestamp))
-    os.mkdir(path_to_backup_dir)
-    delete_backup_dir = True
-    
+
+    # Write table data to CSV file
     for schema in schemas:
         if schema[0] in ignore_schemas:
             continue
-        
-        cursor.execute('SELECT * FROM information_schema.tables WHERE table_schema = \'{}\';'
-                .format(schema[0]))
-        tables = cursor.fetchall() 
+
+        cursor.execute(f"""SELECT * FROM information_schema.tables WHERE table_schema = '{schema[0]}';""")
+
+        tables = cursor.fetchall()
 
         for table in tables:
             if table[2] in ignore_tables:
@@ -39,50 +54,14 @@ def backup_geocml_db():
 
             delete_backup_dir = False
 
-            # Write to schema file
-            schema_file_path = os.path.join(path_to_backup_dir, 'schema:{}.{}.sql'.format(schema[0], table[2]))
-            schema_file = open(schema_file_path, 'w')
-
-            if not schema[0] == 'public':
-                cursor.execute('SELECT DISTINCT grantee FROM information_schema.role_table_grants WHERE table_schema = \'{}\';'
-                        .format(schema[0]))
-                schema_owner = cursor.fetchall()
-                schema_file.write('CREATE SCHEMA IF NOT EXISTS {} AUTHORIZATION {};\n'
-                        .format(schema[0], schema_owner[0][0]))
-
-            cursor.execute('SELECT pg_get_constraintdef(oid) FROM pg_constraint WHERE contype = \'p\' AND conrelid::regclass::text LIKE \'%{}%\';'.format(table[2]))
-
-            pk = cursor.fetchall()
-
-            cursor.execute('SELECT column_name, udt_name FROM information_schema.columns WHERE table_name = \'{}\';'
-                    .format(table[2]))
-
-            columns_and_datatypes = []
-            for row in cursor:
-                if len(row) == 3: # column has a constraint                   
-                    columns_and_datatypes.append('{} {} {}'.format(row[0], row[1], row[2]))
-                else:
-                    columns_and_datatypes.append('{} {}'.format(row[0], row[1]))
-            columns_and_datatypes = ', '.join(columns_and_datatypes)
-
-            if len(pk) > 0: # table has primary key (expected)
-                schema_file.write('CREATE TABLE IF NOT EXISTS {}."{}" ({}, {});\n'.format(schema[0], table[2], columns_and_datatypes, pk[0][0]))
-            else:
-                schema_file.write('CREATE TABLE IF NOT EXISTS {}."{}" ({});\n'.format(schema[0], table[2], columns_and_datatypes))
-
-            cursor.execute('SELECT tableowner FROM pg_tables WHERE tablename = \'{}\';'.format(table[2]))
-            table_owner = cursor.fetchall()
-
-            schema_file.write('ALTER TABLE {}."{}" OWNER TO {};'.format(schema[0], table[2], table_owner[0][0]))
-            schema_file.close()
-             
-            # Write to data file
-            data_file_path = os.path.join(path_to_backup_dir, 'data:{}.{}.csv'.format(schema[0], table[2]))
-            data_file = open(data_file_path, 'w')
-            cursor.copy_expert('COPY {}."{}" TO STDOUT WITH (FORMAT csv, DELIMITER \',\', HEADER FALSE);'.format(schema[0], table[2]), data_file)
+            data_file_path = os.path.join(path_to_backup_dir, "data:{}.{}.csv".format(schema[0], table[2]))
+            data_file = open(data_file_path, "w")
+            cursor.copy_expert(f"""COPY {schema[0]}."{table[2]}" TO STDOUT WITH (FORMAT csv, DELIMITER ',', HEADER);""", data_file)
             data_file.close()
 
     if delete_backup_dir: # nothing to back up
-        path_to_backup_dir.rmdir()
+        log("Nothing to backup")
+        os.rmdir(path_to_backup_dir)
+
     cursor.close()
     conn.close()
