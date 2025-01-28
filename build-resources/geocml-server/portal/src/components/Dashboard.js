b99cac5c61b99b3cf907e327806514e3c9bb46f8
Tristan Damron <tristandamron@protonmail.com>
2025-01-28 09:17:23 -0800

    Do not show dashboards on mobile

--- build-resources/geocml-server/portal/src/components/Dashboard.js
+++ build-resources/geocml-server/portal/src/components/Dashboard.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { ChartPicker } from "./ChartPicker";
 import { Chart, ArcElement, Tooltip, BarElement, LinearScale, CategoryScale } from "chart.js";
 
@@ -11,37 +12,42 @@ Chart.defaults.backgroundColor = "#004aad";
 
 
 export function Dashboard() {
-    return (
-        <div className="px-2 py-2">
-            <h3 className="row justify-content-center py-1">WFS Dashboard</h3>
-            <i className="row justify-content-center py-2">Quantitative analysis for hosted WFS layers</i>
-            <div className="row pb-3">
-                <div className="col">
-                    <ChartPicker />
-                </div>
-                <div className="col">
-                    <ChartPicker />
-                </div>
-            </div>
+    const isMobile = useSelector((state) => state.app.isMobile);
 
-            <div className="row pb-3">
-                <div className="col">
-                    <ChartPicker />
+    if (!isMobile)
+        return (
+            <div className="px-2 py-2">
+                <h3 className="row justify-content-center py-1">WFS Dashboard</h3>
+                <i className="row justify-content-center py-2">Quantitative analysis for hosted WFS layers</i>
+                <div className="row pb-3">
+                    <div className="col">
+                        <ChartPicker />
+                    </div>
+                    <div className="col">
+                        <ChartPicker />
+                    </div>
                 </div>
-                <div className="col">
-                    <ChartPicker />
-                </div>
-            </div>
 
-            <div className="row pb-3">
-                <div className="col">
-                    <ChartPicker />
+                <div className="row pb-3">
+                    <div className="col">
+                        <ChartPicker />
+                    </div>
+                    <div className="col">
+                        <ChartPicker />
+                    </div>
                 </div>
-                <div className="col">
-                    <ChartPicker />
+
+                <div className="row pb-3">
+                    <div className="col">
+                        <ChartPicker />
+                    </div>
+                    <div className="col">
+                        <ChartPicker />
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    else
+        return (<div></div>)
 }
 
